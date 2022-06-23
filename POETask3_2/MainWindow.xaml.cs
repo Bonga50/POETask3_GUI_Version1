b1603c41dff5c410d15c59da4a75d010dbcfe189
Siyabonga Zunguza <siyabonga4767@gmail.com>
2022-06-23 20:26:16 +0200

    Added sorting to the list as well as error catching

--- POETask3_2/MainWindow.xaml.cs
+++ POETask3_2/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace POETask3_2
 {
@@ -24,19 +14,47 @@ namespace POETask3_2
         {
             InitializeComponent();
             lblError.Visibility = Visibility.Collapsed;
-            
+
         }
 
         private void btnExpenseCal_Click(object sender, RoutedEventArgs e)
         {
+            lblError.Visibility = Visibility.Collapsed;
             //list initalize [1](Chand, n.d.)
             List<Expensedata> Expenses = new List<Expensedata>();
+           
             try
             {
                 Expenses.Add(new Expensedata()
                 {
+
                     Expense = "Grocery",
                     Amount = Double.Parse(txtGrocery.Text)
+
+                });
+                Expenses.Add(new Expensedata()
+                {
+                    Expense = "Water and lights",
+                    Amount = Double.Parse(txtWater.Text)
+
+                });
+                Expenses.Add(new Expensedata()
+                {
+                    Expense = "Travel costs (including petrol)",
+                    Amount = Double.Parse(txtTravel.Text)
+
+                });
+                Expenses.Add(new Expensedata()
+                {
+                    Expense = "Cell phone and telephone",
+                    Amount = Double.Parse(txtCellPhone.Text)
+
+                });
+
+                Expenses.Add(new Expensedata()
+                {
+                    Expense = "Other expenses",
+                    Amount = Double.Parse(txtOther.Text)
 
                 });
 
@@ -46,31 +64,9 @@ namespace POETask3_2
 
                 lblError.Visibility = Visibility.Visible;
             }
-            
-            Expenses.Add(new Expensedata()
-            {
-                Expense = "Water and lights",
-                Amount = Double.Parse(txtWater.Text)
 
-            });
-            Expenses.Add(new Expensedata()
-            {
-                Expense = "Travel costs (including petrol)",
-                Amount = Double.Parse(txtTravel.Text)
 
-            });
-            Expenses.Add(new Expensedata()
-            {
-                Expense = "Cell phone and telephone",
-                Amount = Double.Parse(txtCellPhone.Text)
 
-            });
-            Expenses.Add(new Expensedata()
-            {
-                Expense = "Other expenses",
-                Amount = Double.Parse(txtOther.Text)
-
-            });
             //total for all expenses above
             /*Expenses.Add(new Expensedata()
             {
@@ -82,7 +78,10 @@ namespace POETask3_2
                 Double.Parse(txtGrocery.Text)
             });*/
 
-            dataExpense.ItemsSource = Expenses;
+            //[2](Sort list in descending order in C# | Techie Delight, 2022)
+            //the below method sorts the list in decending order using Linq
+            List<Expensedata> sorted = Expenses.OrderByDescending(x => x.Amount).ToList();
+            dataExpense.ItemsSource = sorted;
 
 
 
@@ -93,7 +92,7 @@ namespace POETask3_2
 
     class Expensedata
     {
-       
+
         public String Expense { get; set; }
         public double Amount { get; set; }
     }
@@ -103,4 +102,8 @@ namespace POETask3_2
  * 
  * [1] Chand, M. (n.d.). DataGrid in WPF. C-Sharpcorner. Retrieved June 22, 2022, from
  * https://www.c-sharpcorner.com/UploadFile/mahesh/datagrid-in-wpf/
+ * 
+ * [2] Techie Delight. 2022. Sort list in descending order in C# | Techie Delight. [online] Available at:
+ * <https://www.techiedelight.com/sort-list-in-descending-order-in-csharp/> 
+ * [Accessed 23 June 2022].
  */
