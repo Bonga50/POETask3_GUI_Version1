ac705fc91c03477effdc12b2ba75d66405db0995
Siyabonga Zunguza <siyabonga4767@gmail.com>
2022-06-24 16:47:22 +0200

    added items for car

--- POETask3_2/Rentalwindow.xaml.cs
+++ POETask3_2/Rentalwindow.xaml.cs
@@ -19,18 +19,69 @@ namespace POETask3_2
     /// </summary>
     public partial class Rentalwindow : Window
     {
+        double carTotal = 0;
         public Rentalwindow()
         {
             InitializeComponent();
+            //make the car items appear only when yes is selected 
+            txtCarMake.Visibility = Visibility.Collapsed;
+            txtCarModel.Visibility = Visibility.Collapsed;
+            txtCarPurchase.Visibility = Visibility.Collapsed;
+            txtCardeposit.Visibility = Visibility.Collapsed;
+            txtCarInterest.Visibility = Visibility.Collapsed;
+            txtCarInsuranse.Visibility = Visibility.Collapsed;
+            //make the lables appear only when yes is selected
+            lblCarmake.Visibility = Visibility.Collapsed;
+            lblCarModel.Visibility = Visibility.Collapsed;
+            lblPurchase.Visibility = Visibility.Collapsed;
+            lbldeposit.Visibility = Visibility.Collapsed;
+            lblInterest.Visibility = Visibility.Collapsed;
+            lblInsurance.Visibility = Visibility.Collapsed;
+            //button
+            btnCalculate.Visibility = Visibility.Collapsed;
         }
 
         private void cmbCar_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (cmbCar.SelectedIndex.Equals(0)) 
             {
+                //make the car items appear only when yes is selected 
+                txtCarMake.Visibility = Visibility.Visible;
+                txtCarModel.Visibility = Visibility.Visible;
+                txtCarPurchase.Visibility = Visibility.Visible;
+                txtCardeposit.Visibility = Visibility.Visible;
+                txtCarInterest.Visibility = Visibility.Visible;
+                txtCarInsuranse.Visibility = Visibility.Visible;
+                //make the lables appear only when yes is selected
+                lblCarmake.Visibility = Visibility.Visible;
+                lblCarModel.Visibility = Visibility.Visible;
+                lblPurchase.Visibility = Visibility.Visible;
+                lbldeposit.Visibility = Visibility.Visible;
+                lblInterest.Visibility = Visibility.Visible;
+                lblInsurance.Visibility = Visibility.Visible;
+                btnCalculate.Visibility = Visibility.Visible;
 
-            } else if (cmbCar.SelectedIndex.Equals(1))
+
+            } 
+            else if (cmbCar.SelectedIndex.Equals(1))
             {
+                //make the car items appear only when yes is selected 
+                txtCarMake.Visibility = Visibility.Collapsed;
+                txtCarModel.Visibility = Visibility.Collapsed;
+                txtCarPurchase.Visibility = Visibility.Collapsed;
+                txtCardeposit.Visibility = Visibility.Collapsed;
+                txtCarInterest.Visibility = Visibility.Collapsed;
+                txtCarInsuranse.Visibility = Visibility.Collapsed;
+                //make the lables appear only when yes is selected
+                lblCarmake.Visibility = Visibility.Collapsed;
+                lblCarModel.Visibility = Visibility.Collapsed;
+                lblPurchase.Visibility = Visibility.Collapsed;
+                lbldeposit.Visibility = Visibility.Collapsed;
+                lblInterest.Visibility = Visibility.Collapsed;
+                lblInsurance.Visibility = Visibility.Collapsed;
+                //button
+                btnCalculate.Visibility = Visibility.Collapsed;
+
                 
 
             }
